7ef4490b8be66f1a8e6dac2fc662e384effbb8bf
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-25 01:27:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist, :genre
   
@@ -28,7 +30,16 @@ class Song
   end
   
   def self.genre_count
-    
+    binding.pry
   end
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 end
