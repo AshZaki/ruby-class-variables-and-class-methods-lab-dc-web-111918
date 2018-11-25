dda89bbaf18a3368a85f69a18cd22115973b673c
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-25 02:04:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
   
   def self.genre_count
-    self.genres.each do |genre|
+    @@genres.each do |genre|
       if @@genre_count[genre] == nil
         @@genre_count[genre] = 1
       else
